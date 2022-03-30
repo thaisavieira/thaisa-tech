3ef5b45bbddc414bce60ea3e41b94c82cbae5eb5
Thaísa Vieira <thaisavieira.dev@outlook.com>
2022-03-30 17:41:55 -0300

    mobile animation menu update

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,19 @@
 const btnHamburger = document.querySelector('#btnHamburger')
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 header.addEventListener('click', function(){
     console.log ('click hamburger');
 
-    if(header.classList.contains('open')){
+    if(header.classList.contains('open')){ //Close Hamburger Menu
         header.classList.remove('open');
+        overlay.classList.remove ('fade-in');
+        overlay.classList.add ('fade-out');
     }
-    else {
+    else { //Open Hamburger Menu
         header.classList.add('open');
+        overlay.classList.remove ('fade-out');
+        overlay.classList.add ('fade-in');
     }
     
 });
