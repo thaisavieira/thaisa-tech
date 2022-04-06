d09ee6550bb76f1a1911dfef446e735893536b97
Thaísa Vieira <thaisavieira.dev@outlook.com>
2022-04-06 13:30:48 -0300

    complete mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,26 @@
 const btnHamburger = document.querySelector('#btnHamburger')
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 header.addEventListener('click', function(){
     console.log ('click hamburger');
 
     if(header.classList.contains('open')){ //Close Hamburger Menu
         header.classList.remove('open');
-        overlay.classList.remove ('fade-in');
-        overlay.classList.add ('fade-out');
+        fadeElems.forEach(function(element){
+        element.classList.remove ('fade-in');
+        element.classList.add ('fade-out');
+        });
+        
     }
     else { //Open Hamburger Menu
         header.classList.add('open');
-        overlay.classList.remove ('fade-out');
-        overlay.classList.add ('fade-in');
+        fadeElems.forEach(function(element){
+        element.classList.remove ('fade-out');
+        element.classList.add ('fade-in');   
+        });
+        
     }
     
 });
